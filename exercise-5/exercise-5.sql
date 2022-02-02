73870813a3210e5c067d0fd61ef1ed0971e4e35b
taylor-c-williams <taylor.callan.williams@gmail.com>
2022-02-02 12:37:57 -0800

    ex5 pass

--- exercise-5/exercise-5.sql
+++ exercise-5/exercise-5.sql
@@ -1,1 +1,5 @@
 -- the total run time of all inventory items if they were all played back to back
+-- the total run time of all inventory items if they were all played back to back
+SELECT SUM(film.length) from inventory
+INNER JOIN film
+ON film.film_id = inventory.film_id
